a46ae7e7fcbd3187a8086346f55b6310795529eb
Dominique McCloud <niquelejon@gmail.com>
2017-09-07 00:09:23 +0000

    Aded new layers to the menu and createdseperate menus for Mens and Womens Nikes.  Intertwined menus to work together, entering and exiting. Aded new categories, lifestyle, running, and baskbetbal.

--- lib/nikes/cli.rb
+++ lib/nikes/cli.rb
@@ -2,23 +2,105 @@
 
 class TopNikes::CLI
 
+  def mensmenu
+    puts ""
+    puts ""
+    puts "Nike Mens Footwear:: Select from the following"
+    puts "Lifestyle | Basketball | Running | All | Cancel"
+    mcat = gets.strip.downcase
+      if mcat == "lifestyle"
+        puts ""
+        puts ""
+        @mlifestyle = TopNikes::Nikes.scrape_mens_lifestyle
+        puts ""
+        mensmenu
+      elsif mcat == "basketball"
+        puts ""
+        puts ""
+        @mbasketball = TopNikes::Nikes.scrape_mens_basketball
+        puts ""
+        mensmenu
+      elsif mcat == "running"
+        puts ""
+        puts ""
+        @mrunning = TopNikes::Nikes.scrape_mens_running
+        puts ""
+        mensmenu
+      elsif mcat == "all"
+        puts ""
+        puts ""
+        @allmens = TopNikes::Nikes.scrape_mens_all
+        puts ""
+        mensmenu
+      elsif mcat == "cancel"
+        puts ""
+        puts "Returning to main Home menu"
+        menu
+      else
+        puts ""
+        puts "Sorry! Option not found, please select from menu"
+        mensmenu
+      end
+  end
+
+  def womensmenu
+    puts ""
+    puts ""
+    puts "Nike Womens Footwear:: Select from the following"
+    puts "Lifestyle | Basketball | Running | All | Cancel"
+    wcat = gets.strip.downcase
+      if wcat == "lifestyle"
+        puts ""
+        puts ""
+        @wlifestyle = TopNikes::Nikes.scrape_womens_lifestyle
+        puts ""
+        womensmenu
+      elsif wcat == "basketball"
+        puts ""
+        puts ""
+        @wbasketball = TopNikes::Nikes.scrape_womens_basketball
+        puts ""
+        womensmenu
+      elsif wcat == "running"
+        puts ""
+        puts ""
+        @wrunning = TopNikes::Nikes.scrape_womens_running
+        puts ""
+        womensmenu
+      elsif wcat == "all"
+        puts ""
+        puts ""
+        @allwomens = TopNikes::Nikes.scrape_womens_all
+        puts ""
+        womensmenu
+      elsif wcat == "cancel"
+        puts ""
+        puts "Returning to main Home menu"
+        menu
+      else
+        puts ""
+        puts "Sorry! Option not found, please select from menu"
+        womensmenu
+      end
+  end
+
   def menu
     puts "Welcome to Best Selling Nike's Online"
-    puts "Select Gender (male/female) or type cancel to exit"
+    puts "Select Gender (men/women) or type cancel to exit"
     input = nil
     while input != "exit"
     input = gets.strip.downcase
     case input
-    when "male"
-    @mens = TopNikes::Nikes.scrape_mens
-    when "female"
-    @womens = TopNikes::Nikes.scrape_womens
+    when "men"
+      mensmenu
+    when "women"
+      womensmenu
    when "cancel"
      puts ""
      puts ""
      abort("Canceled: Thanks for visiting. Don't forget, Just Do It.")
     else
-      puts "Invalid selection type Male or Female"
+      puts "Invalid selection type Men or Women"
       menu
     end
   end
--- lib/nikes/nikes.rb
+++ lib/nikes/nikes.rb
@@ -3,9 +3,10 @@ require 'open-uri'
 
 class TopNikes::Nikes
 attr_accessor :name, :price
-
-
-def self.scrape_mens
+#===========================================================================================#
+#MENS MENU OPTIONS::INCLUDES BEST SELLING ITEMS FOR LIFESTYLE, BASKETBALL, AND RUNNING shoes#
+#===========================================================================================#
+def self.scrape_mens_all
   puts ""
   puts ""
   puts "Mens Best Selling Nike Selection as of Today - Nike.com"
@@ -21,7 +22,57 @@ def self.scrape_mens
     end
     end
 
-    def self.scrape_womens
+    def self.scrape_mens_lifestyle
+      puts ""
+      puts ""
+      puts "Mens Best Selling LIFESTYLE Nike Selection as of Today - Nike.com"
+        #setup to open the URL
+        doc = Nokogiri::HTML(open("https://store.nike.com/us/en_us/pw/mens-best-lifestyle-shoes/7puZr3nZoneZpi1Zoi3"))
+        shoes = []
+        doc.css(".grid-item-box").each do |item|
+          item_name = item.css('.product-display-name').text.strip
+          item_price = item.css('.prices').text.strip
+          puts "Item: #{item_name}"
+          puts "Price: #{item_price}"
+          puts "---------------------"
+        end
+        end
+
+        def self.scrape_mens_basketball
+          puts ""
+          puts ""
+          puts "Mens Best Selling BASKETBALL Nike Selection as of Today - Nike.com"
+            #setup to open the URL
+            doc = Nokogiri::HTML(open("https://store.nike.com/us/en_us/pw/mens-best-basketball-shoes/7puZr3nZ8r1Zpi1Zoi3"))
+            shoes = []
+            doc.css(".grid-item-box").each do |item|
+              item_name = item.css('.product-display-name').text.strip
+              item_price = item.css('.prices').text.strip
+              puts "Item: #{item_name}"
+              puts "Price: #{item_price}"
+              puts "---------------------"
+            end
+            end
+
+            def self.scrape_mens_running
+              puts ""
+              puts ""
+              puts "Mens Best Selling RUNNING Nike Selection as of Today - Nike.com"
+                #setup to open the URL
+                doc = Nokogiri::HTML(open("https://store.nike.com/us/en_us/pw/mens-best-running-shoes/7puZr3nZ8yzZpi1Zoi3"))
+                shoes = []
+                doc.css(".grid-item-box").each do |item|
+                  item_name = item.css('.product-display-name').text.strip
+                  item_price = item.css('.prices').text.strip
+                  puts "Item: #{item_name}"
+                  puts "Price: #{item_price}"
+                  puts "---------------------"
+                end
+                end
+#=============================================================================================#
+#WOMENS MENU OPTIONS::INCLUDES BEST SELLING ITEMS FOR LIFESTYLE, BASKETBALL, AND RUNNING shoes#
+#=============================================================================================#
+    def self.scrape_womens_all
       puts ""
       puts ""
       puts "Womens Best Selling Nike Selection as of Today - Nike.com"
@@ -37,4 +88,52 @@ def self.scrape_mens
         end
         end
 
-end
+        def self.scrape_womens_lifestyle
+          puts ""
+          puts ""
+          puts "Womens Best Selling LIFESTYLE Nike Selection as of Today - Nike.com"
+            #setup to open the URL
+            doc = Nokogiri::HTML(open("https://store.nike.com/us/en_us/pw/womens-best-lifestyle-shoes/7ptZr3nZoneZpi1Zoi3"))
+            shoes = []
+            doc.css(".grid-item-box").each do |item|
+              item_name = item.css('.product-display-name').text.strip
+              item_price = item.css('.prices').text.strip
+              puts "Item: #{item_name}"
+              puts "Price: #{item_price}"
+              puts "---------------------"
+            end
+            end
+
+            def self.scrape_womens_basketball
+              puts ""
+              puts ""
+              puts "Womens Best Selling BASKETBALL Nike Selection as of Today - Nike.com"
+                #setup to open the URL
+                doc = Nokogiri::HTML(open("https://store.nike.com/us/en_us/pw/womens-best-basketball-shoes/7ptZr3nZ8r1Zpi1Zoi3"))
+                shoes = []
+                doc.css(".grid-item-box").each do |item|
+                  item_name = item.css('.product-display-name').text.strip
+                  item_price = item.css('.prices').text.strip
+                  puts "Item: #{item_name}"
+                  puts "Price: #{item_price}"
+                  puts "---------------------"
+                end
+                end
+
+                def self.scrape_womens_running
+                  puts ""
+                  puts ""
+                  puts "Womens Best Selling RUNNING Nike Selection as of Today - Nike.com"
+                    #setup to open the URL
+                    doc = Nokogiri::HTML(open("https://store.nike.com/us/en_us/pw/womens-best-running-shoes/7ptZr3nZ8yzZpi1Zoi3"))
+                    shoes = []
+                    doc.css(".grid-item-box").each do |item|
+                      item_name = item.css('.product-display-name').text.strip
+                      item_price = item.css('.prices').text.strip
+                      puts "Item: #{item_name}"
+                      puts "Price: #{item_price}"
+                      puts "---------------------"
+                    end
+                    end
+
+end #ends the class
